27e67f166bbdae9cc2acbab9a4db9043dd005dbb
Ryan Smith <0ryansmith1994@gmail.com>
2013-08-08 00:22:00 +0100

    Removed version control from grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,10 @@
 /* jshint node: true */
 module.exports = function (grunt) {
     'use strict';
-    var pkg = grunt.file.readJSON('package.json');
-
-    // Function to update the version.
-    var setVersion = function (fn) {
-        pkg.version = fn(pkg.version.split('.'));
-        grunt.file.write('package.json', JSON.stringify(pkg, null, 4));
-        grunt.task.run(['jshint', 'concat', 'uglify']);
-    };
 
     // Project configuration.
     grunt.initConfig({
-        pkg: pkg,
+        pkg: grunt.file.readJSON('package.json'),
         uglify: {
             options: {
                 banner: grunt.file.read('src/banner.txt') + '\n'
@@ -45,27 +37,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
     // Tasks.
-    grunt.registerTask('default', function () {
-        setVersion(function (version) {
-            return version[0] + '.' + version[1] + '.' + version[2];
-        });
-    });
-
-    grunt.registerTask('patch', function () {
-        setVersion(function (version) {
-            return version[0] + '.' + version[1] + '.' + (Number(version[2]) + 1);
-        });
-    });
-
-    grunt.registerTask('minor', function () {
-        setVersion(function (version) {
-            return version[0] + '.' + (Number(version[1]) + 1) + '.0';
-        });
-    });
-
-    grunt.registerTask('major', function () {
-        setVersion(function (version) {
-            return (Number(version[0]) + 1) + '.0.0';
-        });
-    });
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 };
