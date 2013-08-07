fe4faf41eb66e1a2028da87fe95a352071c54f48
Ryan Smith <0ryansmith1994@gmail.com>
2013-08-07 15:53:25 +0100

    Updated tests.

--- test/core.js
+++ test/core.js
@@ -1,191 +1,270 @@
 /* jshint maxstatements: false, node: true */
 /* global describe */
 /* global it */
+/* global before */
 (function () {
     'use strict';
-    var promise = require('../src/core.js').promise;
     var expect = require('expect.js');
 
-    describe('promise', function () {
-        var myPromise = promise();
-        it('should have a \'then\' function', function () {
-            expect(myPromise.then).to.be.a('function');
-        });
-        it('should have a \'fulfil\' function', function () {
-            expect(myPromise.fulfil).to.be.a('function');
-        });
-        it('should have a \'reject\' function', function () {
-            expect(myPromise.reject).to.be.a('function');
-        });
-        it('should have a \'state\' function', function () {
-            expect(myPromise.state).to.be.a('function');
-        });
-        it('should have a \'restrict\' function', function () {
-            expect(myPromise.restrict).to.be.a('function');
-        });
-        it('should have a \'then\' function on restricted object', function () {
-            expect(myPromise.restrict().then).to.be.a('function');
-        });
-        it('should have a \'state\' function on restricted object', function () {
-            expect(myPromise.restrict().state).to.be.a('function');
-        });
-    });
-
-    describe('fulfilled promise', function () {
-        var myPromise = promise();
-        var myRestrictedPromise = myPromise.restrict();
-        var fulfilledValue = 10;
-
-        it('should call \'onFulfilled\' with value when `then` is called before fulfilment', function (done) {
-            myPromise.then(function (value) {
-                if (value !== fulfilledValue) { throw 'incorrect value'; }
+    describe('promise()', function () {
+        var promise;
+        var testFnExposure = function (name) {
+            it('should expose a function called ' + name, function () {
+                expect(promise()[name]).to.be.a('function');
+            });
+        };
+        var myArg = 10;
+        var correctCallback = function (done) {
+            return function (arg) {
+                expect(arg).to.equal(myArg);
                 done();
-            }, function (reason) {
-                throw 'called incorrect callback with ' + reason;
+            };
+        };
+        var incorrectCallback = function (arg) {
+            throw 'called incorrect callback with ' + arg;
+        };
+
+        before(function () {
+            promise = require('../src/core.js').promise;
+        });
+
+        testFnExposure('then');
+        testFnExposure('fulfil');
+        testFnExposure('reject');
+        testFnExposure('state');
+        testFnExposure('restrict');
+
+        describe('restrict()', function () {
+            var testFnExposure = function (name) {
+                it('should expose the function called ' + name + ' from the unrestricted promise', function () {
+                    var myPromise = promise();
+                    expect(myPromise.restrict()[name]).to.equal(myPromise[name]);
+                });
+            };
+            testFnExposure('then');
+            testFnExposure('state');
+        });
+
+        describe('state()', function () {
+            var states = ['unfulfilled', 'fulfilled', 'rejected'];
+            it('should return \'' + states[0] + '\' before state is changed', function () {
+                var myPromise = promise();
+                expect(myPromise.state()).to.equal(states[0]);
+            });
+            it('should return \'' + states[1] + '\' once ' + states[1], function () {
+                var myPromise = promise();
+                myPromise.fulfil();
+                expect(myPromise.state()).to.equal(states[1]);
+                myPromise.reject();
+                expect(myPromise.state()).to.equal(states[1]);
+            });
+            it('should return \'' + states[2] + '\' once ' + states[2], function () {
+                var myPromise = promise();
+                myPromise.reject();
+                expect(myPromise.state()).to.equal(states[2]);
+                myPromise.fulfil();
+                expect(myPromise.state()).to.equal(states[2]);
             });
         });
 
-        it('should call \'onFulfilled\' with value when `then` is called before fulfilment on restricted object', function (done) {
-            myRestrictedPromise.then(function (value) {
-                if (value !== fulfilledValue) { throw 'incorrect value'; }
-                done();
-            }, function (reason) {
-                throw 'called incorrect callback with ' + reason;
+        describe('then()', function () {
+            var pass = function (arg) {
+                return arg;
+            };
+            var fail = function (arg) {
+                throw arg;
+            };
+
+            it('should call the fulfilled callback when fulfilled after being called', function (done) {
+                var myPromise = promise();
+                myPromise.then(correctCallback(done), incorrectCallback);
+                myPromise.fulfil(myArg);
             });
-        });
-
-        myPromise.fulfil(fulfilledValue);
-
-        it('should call \'onFulfilled\' with value when `then` is called after fulfilment', function (done) {
-            myPromise.then(function (value) {
-                if (value !== fulfilledValue) { throw 'incorrect value'; }
-                done();
-            }, function (reason) {
-                throw 'called incorrect callback with ' + reason;
+            it('should call the fulfilled callback when fulfilled before being called', function (done) {
+                var myPromise = promise();
+                myPromise.fulfil(myArg);
+                myPromise.then(correctCallback(done), incorrectCallback);
             });
-        });
-
-        it('should call \'onFulfilled\' with value when `then` is called after fulfilment on restricted object', function (done) {
-            myRestrictedPromise.then(function (value) {
-                if (value !== fulfilledValue) { throw 'incorrect value'; }
-                done();
-            }, function (reason) {
-                throw 'called incorrect callback with ' + reason;
+            it('should call the rejected callback when rejected after being called', function (done) {
+                var myPromise = promise();
+                myPromise.then(incorrectCallback, correctCallback(done));
+                myPromise.reject(myArg);
             });
-        });
+            it('should call the rejected callback when rejected before being called', function (done) {
+                var myPromise = promise();
+                myPromise.reject(myArg);
+                myPromise.then(incorrectCallback, correctCallback(done));
+            });
+            it('should expose a function called state', function () {
+                expect(promise().then().state).to.be.a('function');
+            });
+            it('should expose a function called then', function () {
+                expect(promise().then().then).to.be.a('function');
+            });
 
-        it('state should return \'fulfilled\'', function () {
-            expect(myPromise.state()).to.equal('fulfilled');
-        });
-
-        it('state should return \'fulfilled\' on restricted object', function () {
-            expect(myPromise.state()).to.equal('fulfilled');
-        });
-
-        describe('chained promise', function () {
-            it('should call fulfilled with reason in fulfilled chain', function (done) {
-                myPromise.then(function (value) {
-                    return value;
-                }, function (reason) {
-                    throw 'called incorrect callback before chain with ' + reason;
-                }).then(function (value) {
-                    if (value !== fulfilledValue) { throw 'incorrect value'; }
-                    done();
-                }, function (reason) {
-                    throw 'called incorrect callback with ' + reason;
+            describe('state()', function () {
+                var states = ['unfulfilled', 'fulfilled', 'rejected'];
+                var getState = function (stateFn, expectedState, done) {
+                    return function () {
+                        expect(stateFn()).to.equal(expectedState);
+                        done();
+                    };
+                };
+                it('should return \'' + states[0] + '\' before state is changed', function () {
+                    expect(promise().then().state()).to.equal(states[0]);
+                });
+                it('should return \'' + states[1] + '\' once ' + states[1] + ' after ' + states[1], function (done) {
+                    var myPromise = promise().fulfil(myArg).then(pass, fail);
+                    myPromise.then(getState(myPromise.state, states[1], done), incorrectCallback);
+                });
+                it('should return \'' + states[1] + '\' once ' + states[1] + ' after ' + states[2], function (done) {
+                    var myPromise = promise().reject(myArg).then(fail, pass);
+                    myPromise.then(getState(myPromise.state, states[1], done), incorrectCallback);
+                });
+                it('should return \'' + states[2] + '\' once ' + states[2] + ' after ' + states[1], function (done) {
+                    var myPromise = promise().fulfil(myArg).then(fail, pass);
+                    myPromise.then(incorrectCallback, getState(myPromise.state, states[2], done));
+                });
+                it('should return \'' + states[2] + '\' once ' + states[2] + ' after ' + states[2], function (done) {
+                    var myPromise = promise().reject(myArg).then(pass, fail);
+                    myPromise.then(incorrectCallback, getState(myPromise.state, states[2], done));
                 });
             });
-            it('should call rejected with reason in rejected chain', function (done) {
-                myPromise.then(function (value) {
-                    throw value; // Create error for chained promise rejection.
-                }, function (reason) {
-                    throw 'called incorrect callback before chain with ' + reason;
-                }).then(function (value) {
-                    throw 'called incorrect callback with ' + value;
-                }, function (reason) {
-                    if (reason !== fulfilledValue) { throw 'incorrect reason'; }
-                    done();
+            
+            describe('then()', function () {
+                it('should call chained fulfilled callback after calling fulfilled callback', function (done) {
+                    promise().fulfil(myArg).then(pass, fail).then(correctCallback(done), incorrectCallback);
                 });
-            });
-        });
-    });
-
-    describe('rejected promise', function () {
-        var myPromise = promise();
-        var myRestrictedPromise = myPromise.restrict();
-        var rejectionReason = 10;
-
-        it('should call \'onRejected\' with reason when `then` is called before rejection', function (done) {
-            myPromise.then(function (value) {
-                throw 'called incorrect callback with ' + value;
-            }, function (reason) {
-                if (reason !== rejectionReason) { throw 'incorrect reason'; }
-                done();
-            });
-        });
-
-        it('should call \'onRejected\' with reason when `then` is called before rejection on restricted object', function (done) {
-            myRestrictedPromise.then(function (value) {
-                throw 'called incorrect callback with ' + value;
-            }, function (reason) {
-                if (reason !== rejectionReason) { throw 'incorrect reason'; }
-                done();
-            });
-        });
-
-        myPromise.reject(rejectionReason);
-
-        it('should call \'onRejected\' with reason when `then` is called after rejection', function (done) {
-            myPromise.then(function (value) {
-                throw 'called incorrect callback with ' + value;
-            }, function (reason) {
-                if (reason !== rejectionReason) { throw 'incorrect reason'; }
-                done();
-            });
-        });
-
-        it('should call \'onRejected\' with reason when `then` is called after rejection on restricted object', function (done) {
-            myRestrictedPromise.then(function (value) {
-                throw 'called incorrect callback with ' + value;
-            }, function (reason) {
-                if (reason !== rejectionReason) { throw 'incorrect reason'; }
-                done();
-            });
-        });
-
-        it('state should return \'rejected\'', function () {
-            expect(myPromise.state()).to.equal('rejected');
-        });
-
-        it('state should return \'rejected\' on restricted object', function () {
-            expect(myPromise.state()).to.equal('rejected');
-        });
-
-        describe('chained promise', function () {
-            it('should call fulfilled with reason in fulfilled chain', function (done) {
-                myPromise.then(function (value) {
-                    throw 'called incorrect callback before chain with ' + value;
-                }, function (reason) {
-                    return reason;
-                }).then(function (value) {
-                    if (value !== rejectionReason) { throw 'incorrect reason'; }
-                    done();
-                }, function (reason) {
-                    throw 'called incorrect callback with ' + reason;
+                it('should call chained rejected callback after calling fulfilled callback', function (done) {
+                    promise().fulfil(myArg).then(fail, pass).then(incorrectCallback, correctCallback(done));
                 });
-            });
-            it('should call rejected with reason in rejected chain', function (done) {
-                myPromise.then(function (value) {
-                    throw 'called incorrect callback before chain with ' + value;
-                }, function (reason) {
-                    throw reason; // Create error for chained promise rejection.
-                }).then(function (value) {
-                    throw 'called incorrect callback with ' + value;
-                }, function (reason) {
-                    if (reason !== rejectionReason) { throw 'incorrect reason'; }
-                    done();
+                it('should call chained fulfilled callback after calling rejected callback', function (done) {
+                    promise().reject(myArg).then(fail, pass).then(correctCallback(done), incorrectCallback);
+                });
+                it('should call chained rejected callback after calling rejected callback', function (done) {
+                    promise().reject(myArg).then(pass, fail).then(incorrectCallback, correctCallback(done));
                 });
             });
         });
     });
 }());
+
+/*(function () {
+    'use strict';
+    var promise;
+    var expect = require('expect.js');
+
+    describe('promise', function () {
+        var myPromise;
+        var myValue = 10;
+        var correctCallback = function (done) {
+            return function (arg) {
+                expect(arg).to.equal(myValue);
+                done();
+            };
+        };
+        var incorrectCallback = function (arg) {
+            throw 'called incorrect callback with ' + arg;
+        };
+        var testThen = function (restrict) {
+            return function () {
+                var myPromise, myRestrictedPromise;
+
+                beforeEach(function () {
+                    myPromise = promise();
+                    myRestrictedPromise = restrict === true ? myPromise.restrict() : myPromise;
+                });
+
+                it('when fulfilled after being called should call \'onFulfilled\' callback with value', function (done) {
+                    myRestrictedPromise.then(correctCallback(done), incorrectCallback);
+                    myPromise.fulfil(myValue);
+                });
+
+                it('when fulfilled before being called should call \'onFulfilled\' callback with value', function (done) {
+                    myPromise.fulfil(myValue);
+                    myRestrictedPromise.then(correctCallback(done), incorrectCallback);
+                });
+
+                it('when rejected after being called should call \'onRejected\' callback with reason', function (done) {
+                    myRestrictedPromise.then(incorrectCallback, correctCallback(done));
+                    myPromise.reject(myValue);
+                });
+
+                it('when rejected before being called should call \'onRejected\' callback with reason', function (done) {
+                    myPromise.reject(myValue);
+                    myRestrictedPromise.then(incorrectCallback, correctCallback(done));
+                });
+            };
+        };
+        var testState = function (restrict) {
+            return function () {
+                var myPromise, myRestrictedPromise;
+
+                beforeEach(function () {
+                    myPromise = promise();
+                    myRestrictedPromise = restrict === true ? myPromise.restrict() : myPromise;
+                });
+
+                it('when unfulfilled should return \'unfulfilled\'', function () {
+                    expect(myRestrictedPromise.state()).to.equal('unfulfilled');
+                });
+
+                it('when fulfilled should return \'fulfilled\'', function () {
+                    myPromise.fulfil();
+                    expect(myRestrictedPromise.state()).to.equal('fulfilled');
+                    myPromise.reject();
+                    expect(myRestrictedPromise.state()).to.equal('fulfilled');
+                });
+
+                it('when rejected should return \'rejected\'', function () {
+                    myPromise.reject();
+                    expect(myRestrictedPromise.state()).to.equal('rejected');
+                    myPromise.fulfil();
+                    expect(myRestrictedPromise.state()).to.equal('rejected');
+                });
+            };
+        };
+
+        before(function () {
+            promise = require('../src/core.js').promise;
+            myPromise = promise();
+        });
+
+        it('should expose a \'then\' function', function () {
+            expect(myPromise.then).to.be.a('function');
+        });
+        it('should expose a \'fulfil\' function', function () {
+            expect(myPromise.fulfil).to.be.a('function');
+        });
+        it('should expose a \'reject\' function', function () {
+            expect(myPromise.reject).to.be.a('function');
+        });
+        it('should expose a \'state\' function', function () {
+            expect(myPromise.state).to.be.a('function');
+        });
+        it('should expose a \'restrict\' function', function () {
+            expect(myPromise.restrict).to.be.a('function');
+        });
+
+        describe('restricted promise', function () {
+            var myRestrictedPromise;
+
+            before(function () {
+                myRestrictedPromise = promise().restrict();
+            });
+
+            it('should expose a \'then\' function', function () {
+                expect(myPromise.restrict().then).to.be.a('function');
+            });
+            it('should expose a \'state\' function', function () {
+                expect(myPromise.restrict().state).to.be.a('function');
+            });
+
+            describe('then', testThen(true));
+            describe('state', testState(true));
+        });
+
+        describe('then', testThen(false));
+        describe('state', testState(false));
+    });
+}());*/
