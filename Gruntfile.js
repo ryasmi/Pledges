68bc4df128193111e6c68abdfc54264c7dffbff8
Ryan Smith <0ryansmith1994@gmail.com>
2013-09-12 16:10:40 +0100

    Updated gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 /* jshint node: true */
 module.exports = function (grunt) {
     'use strict';
+    var srcFiles = 'src/**/*.js';
+    var testFiles = 'test/**/*.js';
 
     // Project configuration.
     grunt.initConfig({
@@ -10,21 +12,21 @@ module.exports = function (grunt) {
                 banner: grunt.file.read('src/banner.txt') + '\n'
             },
             build: {
-                src: 'build/release.min.js',
-                dest: 'build/release.min.js'
+                src: '<%= pkg.main %>',
+                dest: '<%= pkg.main %>'
             }
         },
         concat: {
             options: {
                 separator: ';'
             },
-            dist: {
-                src: ['src/**/*.js'],
-                dest: 'build/release.min.js'
+            source: {
+                src: srcFiles,
+                dest: '<%= pkg.main %>'
             }
         },
         jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            files: ['Gruntfile.js', srcFiles, testFiles],
             options: {
                 jshintrc: '.jshintrc'
             }
@@ -37,7 +39,9 @@ module.exports = function (grunt) {
                 ui: 'bdd',
                 reporter: 'spec'
             },
-            all: { src: ['test/**/*.js'] }
+            test: {
+                src: testFiles
+            }
         }
     });
 
